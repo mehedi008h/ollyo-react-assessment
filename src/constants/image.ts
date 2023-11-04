8f897bf00e6500e68847c1c1c19265ce135b9493
Mehedi Hasan <mehedi08h@gmail.com>
2023-11-04 12:32:55 +0600

    import & export image

---
+++ src/constants/image.ts
@@ -0,0 +1,25 @@
+import img_1 from "../assets/images/image-1.webp";
+import img_2 from "../assets/images/image-2.webp";
+import img_3 from "../assets/images/image-3.webp";
+import img_4 from "../assets/images/image-4.webp";
+import img_5 from "../assets/images/image-5.webp";
+import img_6 from "../assets/images/image-6.webp";
+import img_7 from "../assets/images/image-7.webp";
+import img_8 from "../assets/images/image-8.webp";
+import img_9 from "../assets/images/image-9.webp";
+import img_10 from "../assets/images/image-10.jpeg";
+import img_11 from "../assets/images/image-11.jpeg";
+
+export default {
+    img_1,
+    img_2,
+    img_3,
+    img_4,
+    img_5,
+    img_6,
+    img_7,
+    img_8,
+    img_9,
+    img_10,
+    img_11,
+};
